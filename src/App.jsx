0a0a45aa95071c9739adc901d1be0966edda1e9c
ehcodes <engrid.hernandez@gmail.com>
2023-09-08 15:09:53 -0400

    added vite base

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className="app"></div>
+  )
+}
