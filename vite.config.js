7c84574bbc54e341b4f649e3fb71ddfc7a032f64
ehcodes <engrid.hernandez@gmail.com>
2023-09-13 22:33:16 -0400

    prep for deployment to gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({
-  plugins: [react()],
-});
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base:'/'
+// });
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/word-lookup/'
+  }
+
+  return config
+})
